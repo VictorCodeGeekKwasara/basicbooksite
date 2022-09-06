b8dec9d84851e6eb094c16a15765b54eb1b683aa
root <root@DESKTOP-87DPNVK.localdomain>
2022-09-06 21:39:52 +0200

    mobile nav

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,56 +1,26 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
+import React from 'react'
+import Header from './header'
+// import { ThemeProvider, createTheme } from "@mui/material/styles"
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: ,
+//     },
+//     secondary: {
+//       main: "#11cb5f",
+//     },
+//   },
+// })
+// theme={theme}
 
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+const Layout = ({children}) => {
+  return (<>
+            <Header/>
+            <div>Layout</div>
+            {children}
+          </>      
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
